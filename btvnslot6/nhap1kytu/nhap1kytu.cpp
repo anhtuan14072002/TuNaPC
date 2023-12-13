f87cfb53040872f82dfbddbd404d1abfd1098064
Nguyễn Tuấn <67007848+anhtuan14072002@users.noreply.github.com>
2023-12-13 13:36:57 +0700

    Add files via upload

---
+++ btvnslot6/nhap1kytu/nhap1kytu.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	char kytu;
+	printf("nhap vao 1 ky tu:");
+	scanf("%c", &kytu);
+	
+	if(kytu >= 'A' &&  kytu <='Z' ){
+		printf("ky tu ban nhap la chu hoa");
+	}else{
+	}if(kytu >= 'a' && kytu <= 'z' ){
+		printf("ky tu ban vua nhap la chu thuong");
+	}else{
+	}if(kytu>='0' && kytu <= '9' ){
+		printf("ky tu ban vua nhap la so");
+	}else{
+	}if (!((kytu >= 'A' && kytu <= 'Z') || (kytu >= 'a' && kytu <= 'z') || (kytu >= '0' && kytu <= '9'))){
+		printf("ky tu ban nhap vao la ky tu dac biet");
+	}
+} 
