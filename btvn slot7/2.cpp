54c7af40891d521f8fdd7f4d13fcb53eadd7f86e
Nguyễn Tuấn <67007848+anhtuan14072002@users.noreply.github.com>
2023-12-15 22:54:37 +0700

    2.cpp

--- btvn slot7/2.cpp
+++ btvn slot7/2.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-//Nh?p 2 s? a,b. TÏm u?c chung l?n nh?t v‡ b? chung nh? nh?t c?a a v‡ b
+//Nh?p 2 s? a,b. T√¨m u?c chung l?n nh?t v√† b? chung nh? nh?t c?a a v√† b
 int main(){
 	
 	int a;
@@ -12,6 +12,7 @@ int main(){
 	
 	int i = 1;
 	int ucln = 1;
+	int bcnn=a*b;
 	
 	while(i <= a && i <= b){
 		if(a % i == 0 && b % i == 0){
@@ -20,15 +21,5 @@ int main(){
 		i++;
 	}	
 	printf("uoc chung lon nhat cua %d va %d la: %d \n ",a,b ,ucln);
-	
-	i=1;
-	int bcnn = 1;
-	
-	while(i <= a && i <= b){
-		if(i % a==0 && i % b==0){
-			bcnn = i;
-		}
-		i++;
-	}
 	printf("boi chung nho nhat cua %d va %d la: %d ",a,b ,bcnn);
 }
