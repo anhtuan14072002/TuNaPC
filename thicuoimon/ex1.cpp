dd29fcda2d0d0527d482e99fb78f65c1ad0e9834
Nguyễn Tuấn <67007848+anhtuan14072002@users.noreply.github.com>
2024-01-05 18:54:01 +0700

    Add files via upload

---
+++ thicuoimon/ex1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	printf("Nhap vao so luong phan tu trong mang :");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	for(int i=0 ;i < n; i++){
+		printf("arr[%d]: ",i);
+		scanf("%d",&arr[i]);
+	}
+	int sochancuoicung = 1;
+	for(int i=0;i < n; i++){
+		if(arr[i] % 2 ==0){
+			sochancuoicung = arr[i];
+		}
+	}
+	if(sochancuoicung != 1){
+		printf("So chan cuoi cung trong mang la: %d",sochancuoicung);
+	}else{
+		printf("mang khong co so chan");
+	}
+}
